881134fee51a6262e571dc755145cc999efb5f82
zhangxinwei <zhangxinwei@aspirecn.com>
2015-11-16 16:41:03 +0800

    update

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -8,8 +10,19 @@ func SayHello(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Hello"))
 }
 
+func loginHandler(w http.ResponseWriter, req *http.Request) {
+	t, err := template.ParseFiles("template/html/login.html")
+	if err != nil {
+		log.Println(err)
+	}
+	t.Execute(w, nil)
+}
+
 func main() {
 	http.HandleFunc("/hello", SayHello)
-	http.ListenAndServe(":8001", nil)
+	http.HandleFunc("/login", loginHandler)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 
 }
